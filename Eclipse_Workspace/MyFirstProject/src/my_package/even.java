0ccf3835cc1b79fcda2a346a914b6277fd8c96f1
Manirathnaamballa <manirathnaamballa@gmail.com>
2026-06-17 16:30:49 +0530

    even

---
+++ Eclipse_Workspace/MyFirstProject/src/my_package/even.java
@@ -0,0 +1,16 @@
+package my_package;
+
+public class even {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=20;i++) {
+			if(i%2==0) {
+		
+			System.out.println(i);
+		}
+			}
+		// TODO Auto-generated method stub
+
+	}
+
+}
