461df7d68c747bc6f4389b6fd5000e993fba155b
Manirathnaamballa <manirathnaamballa@gmail.com>
2026-06-17 16:30:10 +0530

    evenodd

---
+++ Eclipse_Workspace/MyFirstProject/src/my_package/evenodd.java
@@ -0,0 +1,16 @@
+package my_package;
+
+public class evenodd {
+
+	public static void main(String[] args) {
+		int n=5;
+		if(n%2==0) {
+			System.out.println("It is an even number");
+		} else {
+			System.out.println("It is an odd number");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
